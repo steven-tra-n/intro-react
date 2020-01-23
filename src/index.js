adeb5eea71a7130dadb7e96e26a324456622f600
Tran <STRAN@insurance.bisys.org>
2020-01-23 17:49:19 -0500

    Added jumpTo

--- src/index.js
+++ src/index.js
@@ -62,7 +62,15 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNUmber: 0,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) == 0
     });
   }
 
@@ -70,6 +78,17 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+        'Go to move #' + move :
+        'Go to game start';
+      return(
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if(winner){
@@ -85,7 +104,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
