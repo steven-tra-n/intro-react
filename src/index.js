ad96a534576ec7ee9fb8455d6a1388e3d4f95074
Steven Tran <steventran9102@gmail.com>
2020-01-27 19:29:44 -0500

    Added color changing

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ class Game extends React.Component {
         'Go to game start';
       return(
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button onClick={() => this.jumpTo(move)} style={ this.state.stepNumber === move ? {backgroundColor: 'red'} : {}}>{desc}</button>
         </li>
       );
     });
